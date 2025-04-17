4cd02688b24da8646eba7885a5ae018158537104
Akash <akash.katailiha120@gmail.com>
2025-04-17 23:50:45 +0530

    Create a GetBullet() method

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-
+  
 namespace CosmicCuration.Bullets
 {
     public  class BulletPool
@@ -16,6 +16,28 @@ namespace CosmicCuration.Bullets
         {
             this.bulletView = BulletView;
             this.bulletScriptableObject = bulletScriptableObject;
+        }
+
+        public BulletController GetBullet()
+        {
+            if(pooledBullets.Count > 0)
+            {
+                PooledBullets pooledBullet = pooledBullets.Find(item => !item.isUsed);
+                if (pooledBullet != null)
+                {
+                    pooledBullet.isUsed = true;
+                    return pooledBullet.bullet;
+                }
+            }
+            return CreateNewPooledBullet();
+        }
+
+        private BulletController CreateNewPooledBullet()
+        {
+            PooledBullets PooledBullet = new PooledBullets();
+            PooledBullet.bullet = new BulletController(bulletView, bulletScriptableObject);
+            PooledBullet.isUsed = true;
+            return PooledBullet.bullet;
         }
 
         public class PooledBullets
