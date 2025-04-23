af4179e082816208e9756c7ec8fd7e6579325f83
Akash <akash.katailiha120@gmail.com>
2025-04-24 01:16:22 +0530

    Add necessary references to powerup pool

--- Assets/Scripts/PowerUps/PowerUpService.cs
+++ Assets/Scripts/PowerUps/PowerUpService.cs
@@ -8,12 +8,14 @@ namespace CosmicCuration.PowerUps
         private PowerUpScriptableObject powerUpScriptableObject;
         private bool isSpawning;
         private float spawnTimer;
+        private PowerUpPool PowerUpPool;
 
         public PowerUpService(PowerUpScriptableObject powerUpScriptableObject)
         {
             this.powerUpScriptableObject = powerUpScriptableObject;
             spawnTimer = this.powerUpScriptableObject.spawnRate;
             isSpawning = true;
+            PowerUpPool = new PowerUpPool();
         }
 
         public void Update()
@@ -25,6 +27,9 @@ namespace CosmicCuration.PowerUps
                 ResetSpawnTimer();
             }
         }
+
+        public void ReturnPowerUpToPool(PowerUpController powerUpToReturn) => PowerUpPool.ReturnItem(powerUpToReturn);
+        
 
         private void ResetSpawnTimer() => spawnTimer = powerUpScriptableObject.spawnRate;
 
@@ -50,11 +55,11 @@ namespace CosmicCuration.PowerUps
             switch (typeToFetch)
             {
                 case PowerUpType.Shield:
-                    return new Shield(fetchedData);
+                    return (PowerUpController)PowerUpPool.GetPowerUp<Shield>(fetchedData);
                 case PowerUpType.DoubleTurret:
-                    return new DoubleTurret(fetchedData);
+                    return (PowerUpController)PowerUpPool.GetPowerUp<DoubleTurret>(fetchedData);
                 case PowerUpType.RapidFire:
-                    return new RapidFire(fetchedData);
+                    return (PowerUpController)PowerUpPool.GetPowerUp<RapidFire>(fetchedData); 
                 default:
                     throw new Exception($"Failed to Create PowerUpController for: {typeToFetch}");
             }
