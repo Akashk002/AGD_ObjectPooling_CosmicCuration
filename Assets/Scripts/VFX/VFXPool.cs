4acc9a9db0ebe4a2ad58ca40d716c9d2e2feea14
Akash <akash.katailiha120@gmail.com>
2025-04-24 01:40:29 +0530

    Make VFX Pool

---
+++ Assets/Scripts/VFX/VFXPool.cs
@@ -0,0 +1,24 @@
+﻿using CosmicCuration.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmicCuration.VFX
+{
+    internal class VFXPool : GenericObjectPool<VFXController>
+    {
+        private VFXView vfxPrefab;
+        // Constructor
+        
+        public VFXPool(VFXView vfxPrefab)
+        {
+            this.vfxPrefab = vfxPrefab;
+        }
+
+        public VFXController GetVFx() => GetItem<VFXController>();
+
+        protected override VFXController CreateItem<T>() => new VFXController(vfxPrefab);
+    }
+}
--- Assets/Scripts/VFX/VFXService.cs
+++ Assets/Scripts/VFX/VFXService.cs
@@ -5,14 +5,19 @@ namespace CosmicCuration.VFX
 {
     public class VFXService
     {
+        private VFXPool VFXPool;
         private List<VFXData> vfxData = new List<VFXData>();
 
-        public VFXService(VFXScriptableObject vfxScriptableObject) => vfxData = vfxScriptableObject.vfxData;
+        public VFXService(VFXScriptableObject vfxScriptableObject)
+        {
+            VFXPool = new VFXPool(vfxScriptableObject.vfxData[0].prefab);
+            vfxData = vfxScriptableObject.vfxData;
+        }
 
         public void PlayVFXAtPosition(VFXType type, Vector2 spawnPosition)
         {
             VFXView prefabToSpawn = vfxData.Find(item => item.type == type).prefab;
-            VFXController vfxToPlay = new VFXController(prefabToSpawn);
+            VFXController vfxToPlay = VFXPool.GetVFx();
             vfxToPlay.Configure(spawnPosition);
         }
     } 
